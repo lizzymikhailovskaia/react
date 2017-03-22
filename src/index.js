156f7b6ef79aa704f1408479ffca0dbe42d760d5
Lizzy <0572788@gmail.com>
2017-03-21 20:05:52 -0700

    made a video on the pade

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
 const API_KEY ='AIzaSyDRAncj3YwgVdEtddLf0h6BUahIRYrKDAk';
+
 //Create a new component. This component should produce some HTML
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: []};
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      // this.setState({ videos: videos}); only works than same name
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 //Take this component's generate HTML and put it on the page (in the DOM)
